612e91c1de37b75cf6a8db5a54a397f69025bb36
Xlaits Xavier <32484365+Xlaits@users.noreply.github.com>
2018-02-01 22:23:31 -0500

    Created main.cpp
    Created the main.cpp file, which will pull together all other files to allow the program to works. Right now it only contains a sample "Hello World" program, but as functions are written by the other members of the team, I will update this file.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+/* *******************************************************************************************************
+ * This is the main file that will bring all the functions together into a coherent, working program.    *
+ * It will contain all the variables needed to hold the values for the other functions, as well as       *
+ * any other things required for the program's requirements.                                             *
+ *                                                                                                       *
+ * This file will be edited and maintained solely by Andrew Roderigues. Any other edits will be          *
+ * removed.                                                                                              *
+ *********************************************************************************************************/
+
+//Define Function Prototypes
+
+int main()
+{
+		// Define Local Constants
+		
+		// Define Local Variables
+		
+		// Begin Code
+		cout << "Hello world!" << endl;
+		return 0;
+}
